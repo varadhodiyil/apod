c54a31beae51748954a1655539cdbb35d8a999a6
Rajvel CG <cgrvel@gmail.com>
2018-09-27 01:42:35 +0530

    working on setwallpaper

--- android/app/src/main/java/com/example/apod/MainActivity.java
+++ android/app/src/main/java/com/example/apod/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.apod;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.Channel;
+import java.util.Map;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "demo/message";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler(){
+
+      @Override
+      public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+
+        final Map<String,Object> arguments = methodCall.arguments();
+
+        if (methodCall.method.equals("getMessage"))
+        {
+          String path = (String) arguments.get("path");
+          String message = "Its working and the path is -- "+ path;
+          result.success(message);
+          setWallpaper(path);
+        }
+      }
+
+      private void setWallpaper (String path)
+      {
+        File imgFile = new  File(path);
+        Log.e(TAG, "shareFilepathhhhhhhhhhhhh: "+imgFile);
+        // set bitmap to wallpaper
+        Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        WallpaperManager wm = WallpaperManager.getInstance(getApplication().getApplicationContext());
+        try{
+          wm.setBitmap(bitmap);
+        }catch (IOException e){
+          Log.e(TAG, "shareFile: cannot set image as wallpaper",e );
+        }
+      }
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@
 // found in the LICENSE file.
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:apod/saveFile.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import './NasaResponse.dart';
@@ -25,119 +28,123 @@ Future<APODResponse> fetchPost() async {
     return APODResponse.fromJson(json.decode(response.body));
   } else {
     // If that call was not successful, throw an error.
-     throw Exception('Failed to load post');
+    throw Exception('Failed to load post');
   }
 }
 
 class MyApp extends State<MyAppState> {
   StreamController<APODResponse> _refreshController;
+  static const platform = const MethodChannel('demo/message');
+  String _message = "Hellow Rajvel";
+  static APODResponse nasaResponse = new APODResponse();
 
-   static APODResponse nasaResponse = new APODResponse();
-  
-  Future<Null>_getData() async{
-      nasaResponse = await fetchPost();
-     
-    _refreshController.add(nasaResponse);
-    return null;
+  Future<String> _getMessage(String url) async {
+    String value;
+    try {
+      File file = await SaveFile().saveImage(url);
+      var sendMap = <String, dynamic>{'path': file.path};
+      value = await platform.invokeMethod('getMessage', sendMap);
+    } on Error catch (e) {
+      throw 'Error has occured while saving';
+    }
+
+    return value;
   }
 
-   _fetch() {
- 
-    _getData();
-    
+  Future<Null> _getData() async {
+    nasaResponse = await fetchPost();
+    _refreshController.add(nasaResponse);
+    _getMessage(nasaResponse.url).then((String message) {
+      setState(() {
+        _message = message;
+      });
+    });
+    return null;
   }
 
   @override
   void initState() {
     super.initState();
-    
+
     _refreshController = new StreamController<APODResponse>();
-  
+
+    
     _getData();
   }
 
- 
   @override
   Widget build(BuildContext context) {
-
-
-
-    Widget reLoad = Container(
-      
-      child: MaterialButton(
-                minWidth: 200.0,
-                height: 42.0,
-                color: Colors.tealAccent[700],
-                onPressed:(){ _fetch();},
-                child: Text(
-                  'Reload',
-                  style: TextStyle(color: Colors.white),
-                ))
-    );
-
-    
+    // Widget reLoad = Container(
+    //     child: MaterialButton(
+    //         minWidth: 200.0,
+    //         height: 42.0,
+    //         color: Colors.tealAccent[700],
+    //         onPressed: () {
+    //           _fetch();
+    //         },
+    //         child: Text(
+    //           'Reload',
+    //           style: TextStyle(color: Colors.white),
+    //         )));
 
     return new MaterialApp(
       title: "Snow's APOD",
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        appBar: AppBar(
-          title: Text('Apod'),
-        ),
-        body:Container(
-          child: StreamBuilder(
-            stream: _refreshController.stream,
-            builder: (BuildContext context,AsyncSnapshot snapshot){
-            if (snapshot.hasError) {
-            return Text(snapshot.error);
-          }
-          if (snapshot.hasData) {
-              return new Column(
-          children: <Widget>[
-            Expanded(
-                  child: Scrollbar(
-            child: RefreshIndicator(
-                      onRefresh: _getData,
-                child: ListView(
-                  children : <Widget>[        
-           new Image.network(
-              snapshot.data.url,
-              width: 600.0,
-              height: 550.0,
-              fit: BoxFit.cover,
-            ),
-            new Text(
-                    snapshot.data.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  new Text(
-                  snapshot.data.explanation,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-               // reLoad
-                  ]
-                )  
-            )
-                  )
-            )      
-          ],
-        );
-                }
-          if (snapshot.connectionState != ConnectionState.done) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }  
-                       
-            }
+          backgroundColor: Colors.amberAccent,
+          appBar: AppBar(
+            title: Text('Apod'),
           ),
-        )
-         
-      ),
+          body: Container(
+            child: StreamBuilder(
+                stream: _refreshController.stream,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error);
+                  }
+                  if (snapshot.hasData) {
+                    return new Column(
+                      children: <Widget>[
+                        Expanded(
+                            child: Scrollbar(
+                                child: RefreshIndicator(
+                                    onRefresh: _getData,
+                                    child: ListView(children: <Widget>[
+                                      new Image.network(
+                                        snapshot.data.url,
+                                        width: 600.0,
+                                        height: 550.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      new Text(
+                                        snapshot.data.title,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      new Text(
+                                        snapshot.data.explanation,
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                        ),
+                                      ),
+                                      new Text(
+                                        _message,
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                        ),
+                                      ),
+                                      // reLoad
+                                    ]))))
+                      ],
+                    );
+                  }
+                  if (snapshot.connectionState != ConnectionState.done) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+          )),
     );
   }
 }
@@ -145,8 +152,6 @@ class MyApp extends State<MyAppState> {
 class MyAppState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-   
-
     return MyApp();
   }
 }
